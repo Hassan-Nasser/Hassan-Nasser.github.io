2ff955b2ca41347642a16f71427400aabfab84e0
Hassan <hassan.h.nasser@gmail.com>
2026-05-21 15:18:00 +0300

    Update packages and frameworks

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Services.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import unityIcon from "../../images/icons/Unity.png";
+import unrealIcon from "../../images/icons/Unreal.png";
+import arVrIcon from "../../images/icons/AR&VR.png";
+import multiplayerIcon from "../../images/icons/Multiplayer.png";
 
 export function Services(props) {
     return (
@@ -22,7 +26,7 @@ export function Services(props) {
                             <Link className="col-lg-6 col-md-6 col-sm-6 pointer" to="/portfolio?tag=All">
                                 <div className="services__item">
                                     <div className="services__item__icon">
-                                        <img className="imageIcon" src={require("../../images/icons/Unity.png")} alt="" />
+                                        <img className="imageIcon" src={unityIcon} alt="" />
                                     </div>
                                     <h4>Unity</h4>
                                     <p>Used unity game engine for {new Date().getFullYear() - 2017}+ years in different projects,from games to simulations to mobile apps.</p>
@@ -31,7 +35,7 @@ export function Services(props) {
                             <div className="col-lg-6 col-md-6 col-sm-6 " >
                                 <div className="services__item">
                                     <div className="services__item__icon">
-                                        <img className="imageIcon" src={require("../../images/icons/Unreal.png")} alt="" />
+                                        <img className="imageIcon" src={unrealIcon} alt="" />
                                     </div>
                                     <h4>Unreal</h4>
                                     <p>Experienced in unreal engine and capable of developing all kind of AAA games with it, From battle royale games to FPS games to MMO games.</p>
@@ -40,7 +44,7 @@ export function Services(props) {
                             <Link className="col-lg-6 col-md-6 col-sm-6 pointer" to="/portfolio?tag=AR">
                                 <div className="services__item">
                                     <div className="services__item__icon">
-                                        <img className="imageIcon" src={require("../../images/icons/AR&VR.png")} alt="" />
+                                        <img className="imageIcon" src={arVrIcon} alt="" />
                                     </div>
                                     <h4> AR & VR </h4>
                                     <p>Used different frameworks like Photon and Unet with unity to integrate multiplayer to different kind of games,board games with turn based and Host migration multiplayer, and real-time strategy games.</p>
@@ -49,7 +53,7 @@ export function Services(props) {
                             <Link className="col-lg-6 col-md-6 col-sm-6 pointer" to="/portfolio?tag=Multiplayer">
                                 <div className="services__item">
                                     <div className="services__item__icon">
-                                        <img className="imageIcon" src={require("../../images/icons/Multiplayer.png")} alt="" />
+                                        <img className="imageIcon" src={multiplayerIcon} alt="" />
                                     </div>
                                     <h4>Multiplayer</h4>
                                     <p>Developed all kind of AR apps and VR games, used vuforia marker AR to develop multiple mobile apps,used SteamVR to develop multiple PC games and simulations.</p>
